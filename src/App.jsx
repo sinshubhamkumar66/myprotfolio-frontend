0df5e7b3598f86fa727d34941d378ea253848455
shubhamsingh <singhshubhamkumar66@gmail.com>
2026-01-07 00:07:06 +0530

    update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
